f3d8c675314447c27c1e19a87a46ee1487760785
TorJack <124510313+SMartorana@users.noreply.github.com>
2023-11-17 17:54:59 +0100

    Add part-two
    add part-two of the exercise.

--- examples/example.cpp
+++ examples/example.cpp
@@ -6,16 +6,27 @@ int main() {
 
     // Adding functions to the module
     std::unique_ptr<Function> func1 = std::make_unique<Function>("Function1");
-    func1->addBasicBlock(std::make_unique<BasicBlock>("BB1"));
-    func1->addBasicBlock(std::make_unique<BasicBlock>("BB2"));
+    std::unique_ptr<BasicBlock> bb1 = std::make_unique<BasicBlock>("BB1");
+    std::unique_ptr<BasicBlock> bb2 = std::make_unique<BasicBlock>("BB2");
+    std::unique_ptr<BasicBlock> bb3 = std::make_unique<BasicBlock>("BB3");
+
+    bb1->addSuccessor(std::move(bb2), "true");
+    bb1->addSuccessor(std::move(bb3), "false");
+
+    func1->addBasicBlock(std::move(bb1));
+    func1->addBasicBlock(std::move(bb2));
+    func1->addBasicBlock(std::move(bb3));
     myModule->addFunction(std::move(func1));
 
-    std::unique_ptr<Function> func2 = std::make_unique<Function>("Function2");
-    func2->addBasicBlock(std::make_unique<BasicBlock>("BB3"));
-    myModule->addFunction(std::move(func2));
-
-    // Removing a function from the module (if needed)
-    myModule->removeFunction("Function2");
+    // Serialize to GraphViz
+    std::ofstream outputFile("control_flow_graph.dot");
+    if (outputFile.is_open()) {
+        serializeToGraphViz(outputFile, *myModule);
+        outputFile.close();
+        std::cout << "GraphViz file 'control_flow_graph.dot' generated successfully." << std::endl;
+    } else {
+        std::cerr << "Error: Unable to open file for writing." << std::endl;
+    }
 
     return 0;
 }
--- include/compiler.hpp
+++ include/compiler.hpp
@@ -2,94 +2,129 @@
 #include <memory>
 #include <unordered_map>
 #include <vector>
+#include <fstream>
+// namespace ce{
 
-// Forward declarations
-class BasicBlock;
-class Function;
-class Module;
+    // Forward declarations
+    class BasicBlock;
+    class Function;
+    class Module;
 
-// Basic block data structure
-class BasicBlock {
-public:
-    BasicBlock(const std::string& name) : name(name) {}
+    // Basic block data structureclass BasicBlock {
+    class BasicBlock {
 
-    const std::string& getName() const {
-        return name;
-    }
+    public:
+        BasicBlock(const std::string& name) : name(name) {}
 
-private:
-    std::string name;
-};
+        const std::string& getName() const {
+            return name;
+        }
 
-// Function data structure
-class Function {
-public:
-    Function(const std::string& name) : name(name) {}
+        void addSuccessor(std::unique_ptr<BasicBlock> successor, const std::string& tag) {
+            successors.emplace_back(std::move(successor), tag);
+        }
 
-    const std::string& getName() const {
-        return name;
-    }
-
-    void addBasicBlock(std::unique_ptr<BasicBlock> basicBlock) {
-        basicBlocks.push_back(std::move(basicBlock));
-    }
-
-    void removeBasicBlock(const std::string& basicBlockName) {
-        // This is just a placeholder, actual implementation depends on your requirements
-        for (auto it = basicBlocks.begin(); it != basicBlocks.end(); ++it) {
-            if ((*it)->getName() == basicBlockName) {
-                it = basicBlocks.erase(it);
-                --it; // Adjust the iterator to stay at the current position after erase
+        void removeSuccessor(const std::string& successor) {
+            for (auto it = successors.begin(); it != successors.end(); ++it) {
+                if ((*it).first->getName() == successor) {
+                    it = successors.erase(it);
+                    --it; // Adjust the iterator to stay at the current position after erase
+                }
             }
         }
-    }
 
-private:
-    std::string name;
-    std::vector<std::unique_ptr<BasicBlock>> basicBlocks;
-};
-
-// Module data structure
-class Module {
-public:
-    Module(const std::string& name) : name(name) {}
-
-    const std::string& getName() const {
-        return name;
-    }
-
-    void addFunction(std::unique_ptr<Function> function) {
-        functions.emplace(function->getName(), std::move(function));
-    }
-
-    void removeFunction(const std::string& functionName) {
-        auto it = functions.find(functionName);
-        if (it != functions.end()) {
-            functions.erase(it);
+        const std::vector<std::pair<std::unique_ptr<BasicBlock>, std::string>>& getSuccessors() const {
+            return successors;
         }
+
+    private:
+        std::string name;
+        std::vector<std::pair<std::unique_ptr<BasicBlock>, std::string>> successors;
+    };
+
+    // Function data structure
+    class Function {
+    public:
+        Function(const std::string& name) : name(name) {}
+
+        const std::string& getName() const {
+            return name;
+        }
+
+        void addBasicBlock(std::unique_ptr<BasicBlock> basicBlock) {
+            basicBlocks.push_back(std::move(basicBlock));
+        }
+
+        void removeBasicBlock(const std::string& basicBlockName) {
+            // This is just a placeholder, actual implementation depends on your requirements
+            for (auto it = basicBlocks.begin(); it != basicBlocks.end(); ++it) {
+                if ((*it)->getName() == basicBlockName) {
+                    it = basicBlocks.erase(it);
+                    --it; // Adjust the iterator to stay at the current position after erase
+                }
+            }
+        }
+
+        const std::vector<std::unique_ptr<BasicBlock>>& getBasicBlocks() const {
+            return basicBlocks;
+        }
+
+    private:
+        std::string name;
+        std::vector<std::unique_ptr<BasicBlock>> basicBlocks;
+    };
+
+    // Module data structure
+    class Module {
+    public:
+        Module(const std::string& name) : name(name) {}
+
+        const std::string& getName() const {
+            return name;
+        }
+
+        void addFunction(std::unique_ptr<Function> function) {
+            functions.emplace(function->getName(), std::move(function));
+        }
+
+        void removeFunction(const std::string& functionName) {
+            functions.erase(functionName);
+        }
+
+        const std::unordered_map<std::string, std::unique_ptr<Function>>& getFunctions() const {
+            return functions;
+        }
+
+    private:
+        std::string name;
+        std::unordered_map<std::string, std::unique_ptr<Function>> functions;
+    };
+
+    // Serialization routine for GraphViz
+    void serializeToGraphViz(std::ostream& os, const Module& module) {
+        os << "digraph " << module.getName() << " {" << std::endl;
+
+        // Traverse functions
+        for (const auto& functionEntry : module.getFunctions()) {
+            const auto& function = functionEntry.second;
+
+            // Traverse basic blocks in the function
+            for (const auto& basicBlock : function->getBasicBlocks()) {
+                if (basicBlock != nullptr) {
+                    os << "  \"" << function->getName() << "_" << basicBlock->getName() << "\" [label=\"" << basicBlock->getName() << "\"];" << std::endl;
+
+                    // Traverse successors of the basic block
+                    for (const auto& successorEntry : basicBlock->getSuccessors()) {
+                        const auto& successor = successorEntry.first;
+                        const auto& tag = successorEntry.second;
+
+                        os << "  \"" << function->getName() << "_" << basicBlock->getName() << "\" -> \""
+                        << function->getName() << "_" << successor->getName() << "\" [label=\"" << tag << "\"];" << std::endl;
+                    }
+                }
+            }
+        }
+
+        os << "}" << std::endl;
     }
-
-private:
-    std::string name;
-    std::unordered_map<std::string, std::unique_ptr<Function>> functions;
-};
-
-// int main() {
-//     // Example usage
-//     std::unique_ptr<Module> myModule = std::make_unique<Module>("MyModule");
-
-//     // Adding functions to the module
-//     std::unique_ptr<Function> func1 = std::make_unique<Function>("Function1");
-//     func1->addBasicBlock(std::make_unique<BasicBlock>("BB1"));
-//     func1->addBasicBlock(std::make_unique<BasicBlock>("BB2"));
-//     myModule->addFunction(std::move(func1));
-
-//     std::unique_ptr<Function> func2 = std::make_unique<Function>("Function2");
-//     func2->addBasicBlock(std::make_unique<BasicBlock>("BB3"));
-//     myModule->addFunction(std::move(func2));
-
-//     // Removing a function from the module (if needed)
-//     myModule->removeFunction("Function2");
-
-//     return 0;
 // }
